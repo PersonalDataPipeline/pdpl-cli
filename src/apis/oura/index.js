ca18bf56401e25b363f468ab637528fbf7653d12
Josh Cunningham <josh@joshcanhelp.com>
2024-01-12 10:32:53 -0800

    fix: Oura token header

--- src/apis/oura/index.js
+++ src/apis/oura/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   getApiBaseUrl: () => "https://cloud.ouraring.com/v2/",
   getApiAuthHeaders: () => ({
-    Authorization: `Bearer ${`${apiName.toUpperCase()}_AUTH_TOKEN`}`
+    Authorization: `Bearer ${process.env.OURA_AUTH_TOKEN}`
   }),
 }
