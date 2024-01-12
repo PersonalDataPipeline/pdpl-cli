a1aeece7351947f9a153a2238c38299ffb5366d1
Josh Cunningham <josh@joshcanhelp.com>
2024-01-12 10:34:34 -0800

    refactor: Strava auth handler

--- src/apis/strava/getRefreshToken.js
+++ src/apis/strava/getRefreshToken.js
@@ -3,11 +3,22 @@ const http = require("http");
 const crypto = require("crypto");
 const axios = require("axios");
 
+const { authorizeUrl, tokenUrl } = require("./index.js");
+
 const {
   STRAVA_AUTHORIZE_CLIENT_ID,
   STRAVA_AUTHORIZE_CLIENT_SECRET,
   STRAVA_LOCAL_SERVER_PORT
 } = process.env;
+
+const localServerPort = STRAVA_LOCAL_SERVER_PORT ? 
+  parseInt(STRAVA_LOCAL_SERVER_PORT, 10) :
+  8888;
+
+const responseHeaders = {
+  "Content-Type": "text/html",
+  "Cache-Control": "no-cache"
+};
 
 let authorizeState = "";
 
@@ -30,9 +41,7 @@ http.createServer(async (request, response) => {
     if (stateParam !== authorizeState) {
       console.log("Param:" + stateParam);
       console.log("Stored:" + authorizeState);
-      response.writeHead(400, {
-        "Content-Type": "text/html"
-      });
+      response.writeHead(400, responseHeaders);
       response.write("State parameter mis-match.");
       response.write('<br><a href="/">Try again</a>');
       return response.end();
@@ -43,7 +52,7 @@ http.createServer(async (request, response) => {
 
     try {
       tokenResponse = await axios.post(
-        "https://www.strava.com/oauth/token", 
+        tokenUrl, 
         {
           client_id: STRAVA_AUTHORIZE_CLIENT_ID,
           client_secret: STRAVA_AUTHORIZE_CLIENT_SECRET,
@@ -52,36 +61,30 @@ http.createServer(async (request, response) => {
         }
       );
     } catch (tokenError) {
-      response.writeHead(400, {
-        "Content-Type": "text/html"
-      });
+      response.writeHead(400, responseHeaders);
       response.write(`Error exchanging code for token: ${error.message}\n`);
       response.write(JSON.stringify(error.data));
       response.write('<br><a href="/">Try again</a>');
       return response.end();
     }
     
-    response.writeHead(200, {
-      "Content-Type": "text/html"
-    });
+    response.writeHead(200, responseHeaders);
     response.write(`Refresh token for <strong>${tokenResponse.data.athlete.username}</strong>:`);
     response.write(`<br><br><code>${tokenResponse.data.refresh_token}</code>`);
     return response.end();
   }
 
-  const authorizeUrl = new URL("/oauth/authorize", "https://www.strava.com");
-  authorizeUrl.searchParams.append("client_id", STRAVA_AUTHORIZE_CLIENT_ID);
-  authorizeUrl.searchParams.append("redirect_uri", "http://localhost:8989");
-  authorizeUrl.searchParams.append("approval_prompt", "auto");
-  authorizeUrl.searchParams.append("response_type", "code");
-  authorizeUrl.searchParams.append("scope", "read_all,profile:read_all,activity:read_all");
+  const authorizeUrlInstance = new URL(authorizeUrlInstance);
+  authorizeUrlInstance.searchParams.append("client_id", STRAVA_AUTHORIZE_CLIENT_ID);
+  authorizeUrlInstance.searchParams.append("redirect_uri", `http://localhost:${localServerPort}`);
+  authorizeUrlInstance.searchParams.append("approval_prompt", "auto");
+  authorizeUrlInstance.searchParams.append("response_type", "code");
+  authorizeUrlInstance.searchParams.append("scope", "read_all,profile:read_all,activity:read_all");
 
   authorizeState = crypto.randomBytes(16).toString("hex");
-  authorizeUrl.searchParams.append("state", authorizeState);
+  authorizeUrlInstance.searchParams.append("state", authorizeState);
 
-  response.writeHead(200, {
-    "Content-Type": "text/html"
-  });
-  response.write(`<a href="${authorizeUrl.toString()}">Click to authorize</a>`);
+  response.writeHead(200, responseHeaders);
+  response.write(`<a href="${authorizeUrlInstance.toString()}">Click to authorize</a>`);
   response.end();
-}).listen(parseInt(STRAVA_LOCAL_SERVER_PORT, 10));
+}).listen(localServerPort);
