a6fd4d3d670905b05ad1fcd1d2304d1dd6be8534
Josh Cunningham <josh@joshcanhelp.com>
2024-01-12 14:24:29 -0800

    feat: add workout endpoint handler and fix API URL

--- src/apis/oura/index.js
+++ src/apis/oura/index.js
@@ -1,6 +1,26 @@
+const { getFormattedDate } = require("../../utils/date");
+
 module.exports = {
-  getApiBaseUrl: () => "https://cloud.ouraring.com/v2/",
+  getApiBaseUrl: () => "https://api.ouraring.com/v2/",
   getApiAuthHeaders: () => ({
     Authorization: `Bearer ${process.env.OURA_AUTH_TOKEN}`
   }),
+  endpoints: {
+    "usercollection/workout": {
+      method: "get",
+      getParams: () => ({
+        start_date: getFormattedDate(-30),
+        end_date: getFormattedDate(),
+      }),
+      successHandler: (responseData) => {
+        const { data } = responseData;
+        console.log(JSON.stringify(data, null, 2));
+      },
+      errorHandler: (error) => {
+        console.log("❌ Error in handler:");
+        console.log(error.message);
+        console.log(JSON.stringify(error.data || {}));
+      }
+    }
+  }
 }
