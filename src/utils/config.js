21dc9674095a044265cd50cc85ed9eed75b2d0a3
Josh Cunningham <josh@joshcanhelp.com>
2024-01-26 17:05:03 -0800

    feat: fail script if outputDir does not exist

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,14 +1,22 @@
+const { existsSync } = require("fs");
+
 const config = {
   outputDir: "/Users/joshcanhelp/Scripts/cortex/_data",
   compressJson: true,
 };
 
+if (process.env.DEBUG === "true") {
+  config.debug = true;
+  config.compressJson = false;
+  config.outputDir = "/Users/joshcanhelp/Scripts/cortex/_data_debug";
+}
+
+if (!existsSync(config.outputDir)) {
+  console.log(`❌ Output path "${config.outputDir}" does not exist`);
+  process.exit();
+}
+
 const getConfig = () => {
-  if (process.env.DEBUG === "true") {
-    config.debug = true;
-    config.compressJson = false;
-    config.outputDir = "/Users/joshcanhelp/Scripts/cortex/_data_debug";
-  }
   return config;
 };
 
