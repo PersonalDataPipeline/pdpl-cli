6b07f0e309e4c5902a55fa8111ec739f46996401
Josh Cunningham <josh@joshcanhelp.com>
2024-02-28 15:05:06 -0800

    feat: first pass at queue class and tests

---
+++ src/utils/queue.class.spec.ts
@@ -0,0 +1,167 @@
+import getConfig from "./config.js";
+import { pathExists, readFile, writeFile } from "./fs.js";
+jest.mock("./fs.js", () => ({
+  pathExists: jest.fn(),
+  readFile: jest.fn(),
+  writeFile: jest.fn(),
+}));
+
+import Queue from "./queue.class.js";
+
+const outputDir = getConfig().outputDir;
+const queueFilePath = `${outputDir}/API_NAME/_queue.json`;
+
+describe("Class: Queue", () => {
+  it("looks for a queue file when a new instance is created", () => {
+    new Queue("API_NAME");
+    expect(pathExists).toHaveBeenCalledWith(queueFilePath);
+  });
+
+  describe("queue file does not exist", () => {
+    let queue: Queue;
+
+    beforeAll(() => {
+      (pathExists as jest.Mock).mockImplementation(() => false);
+      queue = new Queue("API_NAME");
+    });
+
+    it("creates the queue file", () => {
+      expect(writeFile).toHaveBeenCalledWith(queueFilePath, "[]");
+    });
+
+    it("returns an empty queue", () => {
+      expect(queue.getQueue()).toEqual([]);
+    });
+  });
+
+  describe("queue file exists", () => {
+    let queue: Queue;
+
+    beforeAll(() => {
+      (pathExists as jest.Mock).mockImplementation(() => true);
+      (readFile as jest.Mock).mockImplementation(() => '[{"test": true}]');
+      queue = new Queue("API_NAME");
+    });
+
+    it("reads the existing queue file", () => {
+      expect(readFile).toHaveBeenCalledWith(queueFilePath);
+    });
+
+    it("returns the queue contents", () => {
+      expect(queue.getQueue()).toEqual([{ test: true }]);
+    });
+  });
+
+  describe("entry management", () => {
+    let queue: Queue;
+
+    beforeEach(() => {
+      (pathExists as jest.Mock).mockImplementation(() => false);
+      queue = new Queue("API_NAME");
+      (writeFile as jest.Mock).mockClear();
+    });
+
+    it("returns undefined when queue is empty", () => {
+      expect(queue.getEntry()).toEqual(undefined);
+    });
+
+    it("removes the entry from the queue", () => {
+      queue.addStandardEntry("TEST_TIME");
+      expect(queue.getQueue().length).toEqual(1);
+      queue.getEntry();
+      expect(queue.getQueue().length).toEqual(0);
+    });
+
+    describe("standard", () => {
+      const testEntry = {
+        type: "standard",
+        apiName: "API_NAME",
+        nextRun: "TEST_TIME",
+      };
+
+      beforeEach(() => {
+        queue.addStandardEntry("TEST_TIME");
+      });
+
+      it("writes the queue file", () => {
+        expect(writeFile).toHaveBeenCalledWith(
+          queueFilePath,
+          JSON.stringify([testEntry], null, 2)
+        );
+      });
+
+      it("adds an entry to the queue", () => {
+        expect(queue.getQueue()).toEqual([testEntry]);
+      });
+
+      it("returns the added entry", () => {
+        expect(queue.getEntry()).toEqual(testEntry);
+      });
+    });
+
+    describe("error", () => {
+      const testEntry = {
+        type: "error",
+        apiName: "API_NAME",
+        endpoint: "TEST_ENDPOINT",
+        retryCount: 2,
+      };
+
+      beforeEach(() => {
+        queue.addErrorEntry("TEST_ENDPOINT", 2);
+      });
+
+      it("writes the queue file", () => {
+        expect(writeFile).toHaveBeenCalledWith(
+          queueFilePath,
+          JSON.stringify([testEntry], null, 2)
+        );
+      });
+
+      it("adds an entry to the queue", () => {
+        expect(queue.getQueue()).toEqual([testEntry]);
+      });
+
+      it("returns the added entry", () => {
+        expect(queue.getEntry()).toEqual(testEntry);
+      });
+    });
+
+    describe("historical", () => {
+      const testEntry = {
+        type: "historical",
+        apiName: "API_NAME",
+        endpoints: [
+          {
+            endpoint: "TEST_ENDPOINT",
+            params: "param1=test&param2=test",
+          },
+        ],
+      };
+
+      beforeEach(() => {
+        queue.addHistoricalEntry([
+          {
+            endpoint: "TEST_ENDPOINT",
+            params: "param1=test&param2=test",
+          },
+        ]);
+      });
+
+      it("writes the queue file", () => {
+        expect(writeFile).toHaveBeenCalledWith(
+          queueFilePath,
+          JSON.stringify([testEntry], null, 2)
+        );
+      });
+
+      it("adds an entry to the queue", () => {
+        expect(queue.getQueue()).toEqual([testEntry]);
+      });
+
+      it("returns the added entry", () => {
+        expect(queue.getEntry()).toEqual(testEntry);
+      });
+    });
+  });
+});
---
+++ src/utils/queue.class.ts
@@ -0,0 +1,95 @@
+import * as path from "path";
+import getConfig from "./config.js";
+import { pathExists, readFile, writeFile } from "../utils/fs.js";
+
+////
+/// Types
+//
+
+export interface HistoricalRunEntryEndpoint {
+  endpoint: string;
+  params: string;
+}
+
+export interface HistoricalRunEntry extends RunEntry {
+  endpoints: HistoricalRunEntryEndpoint[];
+}
+
+export interface ErrorRunEntry extends RunEntry {
+  endpoint: string;
+  retryCount: number;
+}
+
+export interface StandardRunEntry extends RunEntry {
+  nextRun: string;
+}
+
+interface RunEntry {
+  apiName: string;
+  type: "standard" | "error" | "historical";
+}
+
+type RunQueue = (StandardRunEntry | ErrorRunEntry | HistoricalRunEntry)[];
+
+////
+/// Export
+//
+
+export default class Queue {
+  apiName: string;
+  queueFile: string;
+  queue: RunQueue;
+
+  constructor(apiName: string) {
+    this.apiName = apiName;
+    this.queueFile = path.join(getConfig().outputDir, apiName, "_queue.json");
+
+    if (!pathExists(this.queueFile)) {
+      writeFile(this.queueFile, "[]");
+      this.queue = [];
+    } else {
+      const queueContents = readFile(this.queueFile);
+      this.queue = JSON.parse(queueContents) as RunQueue;
+    }
+  }
+
+  getQueue() {
+    return this.queue;
+  }
+
+  getEntry() {
+    return this.queue.shift();
+  }
+
+  addStandardEntry(nextRun: string) {
+    this.queue.push({
+      type: "standard",
+      apiName: this.apiName,
+      nextRun,
+    } as StandardRunEntry);
+    this.writeQueue();
+  }
+
+  addErrorEntry(endpoint: string, retryCount: number) {
+    this.queue.push({
+      type: "error",
+      apiName: this.apiName,
+      endpoint,
+      retryCount,
+    } as ErrorRunEntry);
+    this.writeQueue();
+  }
+
+  addHistoricalEntry(endpoints: HistoricalRunEntryEndpoint[]) {
+    this.queue.push({
+      type: "historical",
+      apiName: this.apiName,
+      endpoints,
+    } as HistoricalRunEntry);
+    this.writeQueue();
+  }
+
+  private writeQueue() {
+    writeFile(this.queueFile, JSON.stringify(this.queue, null, 2));
+  }
+}
