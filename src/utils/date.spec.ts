c21c56d1cd930b9035a665da99646b16e36d5ce9
Josh Cunningham <josh@joshcanhelp.com>
2024-03-01 17:46:23 -0800

    test: add date test for day adjustment

--- src/utils/date.spec.ts
+++ src/utils/date.spec.ts
@@ -9,4 +9,8 @@ describe("Function: getFormattedDate", () => {
     const thisDate = new Date("1990-12-20T23:59:59");
     expect(getFormattedDate(0, thisDate)).toEqual("1990-12-20");
   });
+  it("respects date adjustment for specific date", () => {
+    const thisDate = new Date("2020-01-02 GMT-0800");
+    expect(getFormattedDate(-1, thisDate)).toEqual("2020-01-01");
+  });
 });
