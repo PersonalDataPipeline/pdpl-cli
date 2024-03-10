409d8146b17d6a34cd6eca5c4b367aca8ee295de
Josh Cunningham <josh@joshcanhelp.com>
2024-03-10 11:58:58 -0700

    feat: add API handler output script

---
+++ src/scripts/apis.ts
@@ -0,0 +1,69 @@
+import { config } from "dotenv";
+config();
+import path from "path";
+
+import { readDirectory } from "../utils/fs.js";
+import { ApiHandler } from "../utils/types.js";
+import getConfig from "../utils/config.js";
+
+const apisSupported = readDirectory("./src/apis");
+
+for (const apiName of apisSupported) {
+  if (process.argv[2] && process.argv[2] !== apiName) {
+    continue;
+  }
+
+  const apiHandler = (await import(`../apis/${apiName}/index.js`)) as ApiHandler;
+
+  console.log(`👉 API handler for ${apiHandler.getApiName()}`);
+  console.log(`Base URL: ${apiHandler.getApiBaseUrl()}`);
+
+  let authHeaders: object;
+  try {
+    authHeaders = await apiHandler.getApiAuthHeaders();
+    console.log(`Auth headers: ${JSON.stringify(authHeaders)}`);
+  } catch (error) {
+    console.log(
+      `Error while getting auth headers: ${
+        error instanceof Error ? error.message : "unknown"
+      }`
+    );
+  }
+
+  if (apiHandler.authorizeEndpoint) {
+    console.log(`Authorize endpoint: ${apiHandler.authorizeEndpoint}`);
+  }
+
+  if (apiHandler.tokenEndpoint) {
+    console.log(`Token endpoint: ${apiHandler.tokenEndpoint}`);
+  }
+
+  console.log(`Primary endpoints:`);
+  for (const endpoint of apiHandler.endpointsPrimary) {
+    const params = typeof endpoint.getParams === "function" ? endpoint.getParams() : {};
+    const histParams =
+      typeof endpoint.getHistoricParams === "function"
+        ? endpoint.getHistoricParams()
+        : params;
+    const histDelay =
+      typeof endpoint.getHistoricDelay === "function"
+        ? endpoint.getHistoricDelay()
+        : endpoint.getDelay();
+    console.log(``);
+    console.log(`  | Endpoint: ${endpoint.getEndpoint()}`);
+    console.log(
+      `  | Standard params: ${new URLSearchParams(params as Record<string, string>).toString()}`
+    );
+    console.log(`  | Standard delay in seconds: ${endpoint.getDelay()}`);
+    console.log(
+      `  | Historic params: ${new URLSearchParams(histParams as Record<string, string>).toString()}`
+    );
+    console.log(`  | Historic delay in seconds: ${histDelay}`);
+    console.log(
+      `  | Directory: ${path.join(getConfig().outputDir, apiName, endpoint.getDirName())}`
+    );
+  }
+  console.log(``);
+  console.log(`---`);
+  console.log(``);
+}
