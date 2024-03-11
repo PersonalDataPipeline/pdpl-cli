b17d190a2da52a8ea69d7648e1c4d046d24d9fc8
Josh Cunningham <josh@joshcanhelp.com>
2024-03-11 09:56:11 -0700

    chore: fix pre-commit instructions

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
 # Add this to your git commits:
-# cp pre-commit.sh .git/hooks/pre-commit
+#
+# $ cp pre-commit.sh .git/hooks/pre-commit
+# $ chmod ug+x .git/hooks/*
 
 set -eo pipefail
 npm run build
