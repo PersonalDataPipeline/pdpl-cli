21b859510605526fa9f27225da93b11679a58378
Josh Cunningham <josh@joshcanhelp.com>
2024-04-10 11:10:21 -0700

    feat: migrate API info to oclif command

---
+++ src/commands/api/info.ts
@@ -0,0 +1,55 @@
+import { ApiHandler, EpHistoric } from "../../utils/types.js";
+import { BaseCommand, apiNameArg } from "./_base.js";
+import path from "path";
+
+export default class Logs extends BaseCommand<typeof Logs> {
+  static override summary = "Display info for a specific API";
+  static override examples = ["<%= config.bin %> <%= command.id %> API_NAME"];
+
+  static override args = {
+    ...apiNameArg,
+  };
+
+  public override async run(): Promise<void> {
+    const { default: apiHandler } = (await import(
+      `../../apis/${this.args.apiName}/index.js`
+    )) as {
+      default: ApiHandler;
+    };
+
+    console.log(`API handler for ${apiHandler.getApiName()}`);
+    console.log(`Base URL: ${apiHandler.getApiBaseUrl()}`);
+
+    if (apiHandler.authorizeEndpoint) {
+      console.log(`Authorize endpoint: ${apiHandler.authorizeEndpoint}`);
+    }
+
+    if (apiHandler.tokenEndpoint) {
+      console.log(`Token endpoint: ${apiHandler.tokenEndpoint}`);
+    }
+
+    console.log(`Primary endpoints:`);
+    for (const endpoint of apiHandler.endpointsPrimary) {
+      const params = typeof endpoint.getParams === "function" ? endpoint.getParams() : {};
+      const histParams = endpoint.isHistoric()
+        ? (endpoint as EpHistoric).getHistoricParams()
+        : params;
+      const histDelay = endpoint.isHistoric()
+        ? (endpoint as EpHistoric).getHistoricDelay()
+        : endpoint.getDelay();
+      console.log(``);
+      console.log(`  | Endpoint: ${endpoint.getEndpoint()}`);
+      console.log(
+        `  | Standard params: ${new URLSearchParams(params as Record<string, string>).toString()}`
+      );
+      console.log(`  | Standard delay in seconds: ${endpoint.getDelay()}`);
+      console.log(
+        `  | Historic params: ${new URLSearchParams(histParams as Record<string, string>).toString()}`
+      );
+      console.log(`  | Historic delay in seconds: ${histDelay}`);
+      console.log(
+        `  | Directory: ${path.join(this.conf.outputDir, this.args.apiName, endpoint.getDirName())}`
+      );
+    }
+  }
+}
