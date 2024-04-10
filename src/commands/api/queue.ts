a70c01a291a4599e6c503ccdf7a883ec7568247c
Josh Cunningham <josh@joshcanhelp.com>
2024-04-10 11:38:22 -0700

    fix: command description and param output

--- src/commands/api/queue.ts
+++ src/commands/api/queue.ts
@@ -2,10 +2,9 @@ import CliTable3 from "cli-table3";
 import { ApiHandler } from "../../utils/types.js";
 import { BaseCommand, apiNameArg } from "./_base.js";
 import * as queue from "../../utils/queue.js";
-import { getFormattedDate, getFormattedTime } from "../../utils/date-time.js";
 
 export default class Logs extends BaseCommand<typeof Logs> {
-  static override summary = "Output API queue";
+  static override summary = "Show the queue for an API";
 
   static override examples = ["<%= config.bin %> <%= command.id %> API_NAME"];
 
@@ -15,7 +14,7 @@ export default class Logs extends BaseCommand<typeof Logs> {
 
   public override async run(): Promise<void> {
     const table = new CliTable3({
-      head: ["Endpoint", "Next run", "Historic", "Params"],
+      head: ["Endpoint", "Next run (your time)", "Historic", "Params"],
     });
 
     const { default: handler } = (await import(
@@ -27,17 +26,17 @@ export default class Logs extends BaseCommand<typeof Logs> {
     queue.loadQueue(handler);
 
     for (const entry of queue.getQueue()) {
-      const runAfterDate = new Date(entry.runAfter * 1000);
       table.push([
         entry.endpoint,
-        getFormattedDate(0, runAfterDate) + " " + getFormattedTime(runAfterDate),
+        new Date(entry.runAfter * 1000).toLocaleString(),
         entry.historic ? "yes" : "no",
         JSON.stringify(entry.params)
-          .replaceAll('","', "\n")
-          .replaceAll('":"', ": ")
-          .replaceAll('{"', "")
-          .replaceAll('"}', "")
-          .replace("{}", "None"),
+          .replaceAll(",", "\n")
+          .replaceAll('":', " = ")
+          .replaceAll('"', "")
+          .replace("{}", "None")
+          .replaceAll("{", "")
+          .replaceAll("}", ""),
       ]);
     }
 
