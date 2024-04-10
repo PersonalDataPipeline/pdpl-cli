81b79fe134c47a0d761e47348d2126bff7a49c9b
Josh Cunningham <josh@joshcanhelp.com>
2024-04-10 12:02:03 -0700

    feat: migrate curl command to oclif

---
+++ src/commands/api/curl.ts
@@ -0,0 +1,33 @@
+import { ApiHandler } from "../../utils/types.js";
+import { BaseCommand, apiNameArg } from "./_base.js";
+
+export default class Logs extends BaseCommand<typeof Logs> {
+  static override summary = "Build cURL commands for all API endpoints";
+
+  static override examples = ["<%= config.bin %> <%= command.id %> API_NAME"];
+
+  static override args = {
+    ...apiNameArg,
+  };
+
+  public override async run(): Promise<void> {
+    const { default: apiHandler } = (await import(
+      `../../apis/${this.args.apiName}/index.js`
+    )) as {
+      default: ApiHandler;
+    };
+
+    let curlCommand = "curl";
+
+    const authHeaders: { [key: string]: string } = await apiHandler.getApiAuthHeaders();
+    for (const header in authHeaders) {
+      curlCommand += ` -H "${header}: ${authHeaders[header]}"`;
+    }
+
+    for (const epHandler of apiHandler.endpointsPrimary) {
+      console.log(
+        `${curlCommand} ${apiHandler.getApiBaseUrl() + epHandler.getEndpoint()}`
+      );
+    }
+  }
+}
