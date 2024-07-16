0dae33c062f4e2768b8e5d813f5043a06b8524f4
Josh Cunningham <josh@joshcanhelp.com>
2024-07-16 09:35:23 -0700

    feat: Reddit API authorization

--- src/apis/reddit/index.ts
+++ src/apis/reddit/index.ts
@@ -1,6 +1,9 @@
+import axios, { AxiosResponse } from "axios";
 import { AuthorizeServerConfig } from "../../commands/api/authorize.js";
 import { ONE_DAY_IN_SEC } from "../../utils/date-time.js";
 import { ApiHandler, EpHistoric, EpSecondary, EpSnapshot } from "../../utils/types.js";
+import { makeBasicAuth } from "../../utils/string.js";
+import { envWrite } from "../../utils/fs.js";
 
 const {
   REDDIT_AUTHORIZE_CLIENT_ID = "",
@@ -24,9 +27,39 @@ const isReady = () => !!REDDIT_REFRESH_TOKEN;
 const tokenEndpoint = "https://www.reddit.com/api/v1/access_token";
 const getApiName = () => "reddit";
 const getApiBaseUrl = () => "https://oauth.reddit.com/api/v1/";
-const getApiAuthHeaders = async () => ({
-  Authorization: `Bearer ${REDDIT_REFRESH_TOKEN}`,
-});
+
+let accessToken = "";
+const getApiAuthHeaders = async () => {
+  let tokenResponse: AxiosResponse;
+  const basicAuth = makeBasicAuth(
+    REDDIT_AUTHORIZE_CLIENT_ID,
+    REDDIT_AUTHORIZE_CLIENT_SECRET
+  );
+  if (!accessToken) {
+    tokenResponse = await axios.post(
+      tokenEndpoint,
+      `grant_type=refresh_token&refresh_token=${REDDIT_REFRESH_TOKEN}`,
+      {
+        headers: {
+          "Authorization": `Basic ${basicAuth}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    if (tokenResponse.data.error) {
+      throw new Error(`Error during token refresh: ${tokenResponse.data.error}`);
+    }
+
+    accessToken = tokenResponse.data.access_token;
+    const newRefreshToken = tokenResponse.data.refresh_token;
+    envWrite("REDDIT_REFRESH_TOKEN", REDDIT_REFRESH_TOKEN, newRefreshToken);
+  }
+
+  return {
+    Authorization: `bearer ${accessToken}`,
+  };
+};
 
 const getAuthorizeConfig = (): AuthorizeServerConfig => ({
   checkState: true,
